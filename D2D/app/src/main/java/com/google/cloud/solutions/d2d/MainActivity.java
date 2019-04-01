c656ff84e0526e4e0453c8757b83441742556dbf
iggy <iggy@iggys-MacBook-Pro.local>
2019-04-01 14:59:14 -0400

    Simple app to test connecting to firebase db. Changed some dependencies and updated version numbers for Google services.

--- D2D/app/src/main/java/com/google/cloud/solutions/d2d/MainActivity.java
+++ D2D/app/src/main/java/com/google/cloud/solutions/d2d/MainActivity.java
@@ -1,52 +1,63 @@
 package com.google.cloud.solutions.d2d;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+   Button getButton;
+   Button sendButton;
+   TextView userNameField;
+   TextView userBdayField;
+   TextView userPlanField;
+
+   public static class User {
+       public String name;
+       // Age usually won't be above 127 so byte type can help save some space
+       public byte age;
+       public float bmi;
+       // Java has a Date class that can be used to create date variables but I don't know how it
+       // would play with Firebase date types so I'm leaving it as a string for now and we can
+       // parse through it to convert it to a Java date type
+       public String bday;
+       public String height;
+       public String login;
+       public String password;
+       public int userID;
+       public int weight;
+       public int nutPlan;
+   }
+
+
+
+
+   private DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+   private DatabaseReference userRef = rootRef.child("user");
+   private DatabaseReference bdayRef = rootRef.child("user").child("bday");
+   private DatabaseReference planRef = rootRef.child("user").child("bday").child("nutPlan");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        // UI elements
+        getButton = (Button)findViewById(R.id.getButton);
+        sendButton= (Button)findViewById(R.id.sendButton);
+        userNameField = (TextView)findViewById(R.id.userNameField);
+        userBdayField = (TextView)findViewById(R.id.userBdayField);
+        userPlanField = (TextView)findViewById(R.id.userPlanField);
     }
 }
