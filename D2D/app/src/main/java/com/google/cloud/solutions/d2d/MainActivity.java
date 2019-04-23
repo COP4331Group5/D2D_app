9bb1751d500323da1d515bbd3d44494bf8b6f89b
Michael Burleson <mburleson@cfl.rr.com>
2019-04-23 19:05:40 -0400

    Added submit_nutrition_data()

--- D2D/app/src/main/java/com/google/cloud/solutions/d2d/MainActivity.java
+++ D2D/app/src/main/java/com/google/cloud/solutions/d2d/MainActivity.java
@@ -75,22 +75,113 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return loadFragment(fragment);
     }
 
-    public void submit_Cardio(View view)
-    {
-        EditText cardio_minutes_entry = (EditText) findViewById(R.id.cardio_minutes_entry);
-
-        if(cardio_minutes_entry.getText().toString() != "") {
-            try {
-                int cardio_minutes = Integer.parseInt(cardio_minutes_entry.getText().toString());
+        public void submit_nutrition_data(View view)
+        {
+            EditText calories_consumed_entry = findViewById(R.id.calories_consumed);
+            EditText carbs_consumed_entry = findViewById(R.id.carbs_consumed);
+            EditText sugars_consumed_entry = findViewById(R.id.sugars_consumed);
+            EditText proteins_consumed_entry = findViewById(R.id.proteins_consumed);
+            EditText fats_consumed_entry = findViewById(R.id.fats_consumed);
+            EditText fiber_consumed_entry = findViewById(R.id.fiber_consumed);
+            EditText water_consumed_entry = findViewById(R.id.water_consumed);
+    
+            int calories_consumed, carbs_consumed, sugar_consumed, proteins_consumed, fats_consumed, fiber_consumed, water_consumed;
+    
+            if(calories_consumed_entry.getText().toString() != ""){
+            try{
+                calories_consumed = Integer.parseInt(calories_consumed_entry.getText().toString());
                 //throw DB
-                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("cardio_minutes").setValue(cardio_minutes);
-            } catch (Exception e) {
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("calories_consumed").setValue(calories_consumed);
+            }
+            catch(Exception e)
+            {
                 // This will toast to the screen "You done fucked up!"
-                Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+            }
+            if(carbs_consumed_entry.getText().toString() != ""){
+            try{
+                carbs_consumed = Integer.parseInt(carbs_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("carbs_consumed").setValue(carbs_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
                 // return;
             }
         }
+            if(sugars_consumed_entry.getText().toString() != ""){
+            try{
+                sugar_consumed = Integer.parseInt(sugars_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("sugar_consumed").setValue(sugar_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+        }
+            if(proteins_consumed_entry.getText().toString() != ""){
+            try{
+                proteins_consumed = Integer.parseInt(proteins_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("proteins_consumed").setValue(proteins_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+        }
+            if(fats_consumed_entry.getText().toString() != ""){
+            try{
+                fats_consumed = Integer.parseInt(fats_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("fats_consumed").setValue(fats_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+        }
+            if(fiber_consumed_entry.getText().toString() != ""){
+            try{
+                fiber_consumed = Integer.parseInt(fiber_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("fiber_consumed").setValue(fiber_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+        }
+            if(water_consumed_entry.getText().toString() != ""){
+            try{
+                water_consumed = Integer.parseInt(water_consumed_entry.getText().toString());
+                //throw DB
+                rootRef.child("Users/" + uid + "/Dates").child(getCurrentDate()).child("water_consumed").setValue(water_consumed);
+            }
+            catch(Exception e)
+            {
+                // This will toast to the screen "You done fucked up!"
+                // Toast.makeText(getApplicationContext(), "Incorrect format", Toast.LENGTH_SHORT).show();
+                // return;
+            }
+        }
+        }
     }
+    
+    
 
     public static String getCurrentDate() {
         Calendar now = Calendar.getInstance();
